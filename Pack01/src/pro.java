bd7e006f2ea6cf12a5b35717ac2e2d5b9d51a351
BaDaBaBo <ehddhks881004@gmail.com>
2018-04-23 17:38:30 +0900

    input1과 input2를 입력받아서 더하고 빼기위한 연산
    sum 으로 더하고 sub 로 뺀다

--- Pack01/src/pro.java
+++ Pack01/src/pro.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 
 public class pro {
 	public static void main(String[] args) {
-		System.out.println();
-		System.out.println(2);
+		int sum;
+		int sub;
+		int input1= new Scanner(System.in).nextInt();
+		int input2= new Scanner(System.in).nextInt();
+		
+		sum = input1 + input2;
+		System.out.println(sum);
+		
+		sub = input1 - input2;
+		System.out.println(sub);
 	}
 }
